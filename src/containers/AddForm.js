37419064ef923c9f263d97641013ce26d8628b03
hilmihs <hilmisobandi@gmail.com>
2022-08-20 13:56:48 +0700

    add form on progress

--- src/containers/AddForm.js
+++ src/containers/AddForm.js
@@ -1,9 +1,71 @@
-import { Component } from 'react';
+import React, { useState } from "react"
+import { useDispatch } from 'react-redux';
 import * as Icon from 'react-bootstrap-icons';
 import { connect } from "react-redux"
 import { addPhonebook } from '../actions/phonebooks'
+import { updatePhonebook } from "../features/phonebook/phonebookSlice";
 
-class AddForm extends Component {
+export default function addForm (props) {
+
+    const dispatch = useDispatch();
+
+    const [isAdd, setIsAdd] = useState(false)
+    const [phonebook, setPhonebook] = useState({
+        name: props.phonebook.name,
+        phone: props.phonebook.phone
+    })
+
+    const handleInputChange = (event) => {
+        const target = event.target
+        const value = target.type === 'checkbox' ? target.checked : target.value;
+        const name = target.name;
+
+        setPhonebook({
+            ...phonebook,
+            [name]: value
+        })
+    }
+
+    const handleUpdate = () => {
+        dispatch(updatePhonebook({ id: props.phonebook.id, ...phonebook}))
+        setIsAdd(false)
+    }
+
+    if (isAdd) {
+        return (
+            <tr>
+                <td>{props.no}</td>
+                <td>
+                    <input type="text" name="name" value={phonebook.name} className="form-control" onChange={handleInputChange} />
+                </td>
+                <td>
+                <input type="number" name="phone" value={phonebook.phone} className="form-control" onChange={handleInputChange} />
+                </td>
+                <td>
+                    <button type="button" className='btn btn-primary' onClick={handleUpdate}>save</button>
+                    <button type="button" className='btn btn-warning' onClick={() => setIsEdit(false)}>cancel</button>
+                </td>
+            </tr >
+        )
+    } else {
+        <tr>
+        <td>{props.no}</td>
+        <td>{props.phonebook.name}</td>
+        <td>{props.phonebook.phone}</td>
+        <td>
+            <button type="button" className="btn btn-success" onClick={() => setIsEdit(true)}>edit</button>
+            <button
+                type="button"
+                className={props.todo.sent ? 'btn btn-danger' : 'btn btn-warning'}
+                onClick={props.todo.sent ? () => dispatch(removeTodo(props.todo._id)) : () => dispatch(resendTodo(props.todo))}>
+                {props.todo.sent ? 'hapus' : 'kirim ulang'}
+            </button>
+        </td>
+    </tr >
+    }
+}
+
+class AddForma extends Component {
     constructor(props) {
         super(props);
 
@@ -43,4 +105,4 @@ const mapDispatchToProps = (dispatch, ownProps) => ({
     add: (name, phone) => dispatch(addPhonebook(name, title))
 })
 
-export default connect(mapStateToProps, mapDispatchToProps)(AddForm)
+// export default connect(mapStateToProps, mapDispatchToProps)(AddForm)
--- src/features/phonebook/Layout.js
+++ src/features/phonebook/Layout.js
@@ -1,10 +1,10 @@
 import { render } from "@testing-library/react"
 import React from "react"
-import Header from "./Header"
-import AddButton from "./AddButton"
-import AddForm from "../containers/AddForm"
-import SearchForm from "../containers/SearchForm"
-import TableList from "../containers/TableList"
+import Header from "../../components/Header"
+import AddButton from "../../components/AddButton"
+import AddForm from "../../containers/AddForm"
+import SearchForm from "../../containers/SearchForm"
+import TableList from "../../containers/TableList"
 export default function Layout (props) {
    
         return (
